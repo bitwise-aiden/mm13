9020223d28e49fb04ee9fdd894b088040915b639
Aiden Storey <git@storey.dev>
2021-08-29 22:45:14 -0400

    Add base health controller and player variant

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+class HealthController : MonoBehaviour
+{
+    public delegate void OnDamaged(HealthController self, int health);
+    public OnDamaged onDamaged;
+
+    public delegate void OnDeath(HealthController self);
+    public OnDeath onDeath;
+
+    public int maxHealth = 1;
+    protected int health = 0;
+
+
+    // Lifecycle methods
+
+    void Start()
+    {
+        this.health = this.maxHealth;
+    }
+
+
+    // Public methods
+
+    public void damage(int amount)
+    {
+        this.health = Mathf.Max(0, this.health - amount);
+
+        if (this.onDamaged != null)
+        {
+            this.onDamaged(this, this.health);
+        }
+
+        if (this.health == 0 && this.onDeath != null)
+        {
+            this.onDeath(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+
+class PlayerHealthController : HealthController
+{
+    // Lifecycle Methods
+
+    void Start()
+    {
+        this.onDamaged += this.debugDamaged;
+        this.onDeath += this.debugDeath;
+        this.onDeath += this.death;
+    }
+
+
+    // Callback methods
+    void death(HealthController self)
+    {
+        this.health = this.maxHealth;
+    }
+
+
+    // Test methods
+
+    void debugDamaged(HealthController self, int health)
+    {
+        Debug.Log("Damaged, health remaining: " + health);
+    }
+
+    void debugDeath(HealthController self)
+    {
+        Debug.Log("Deaded");
+    }
+}
