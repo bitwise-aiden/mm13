f9060e883c59381346611908ad34414e339deb59
Liioni <40169289+Liioni@users.noreply.github.com>
2021-09-03 20:28:57 +0200

    changed how doors work

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+    [SerializeField] List<GameObject> connectedDoors;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Player"){
-            GameObject door = transform.parent.transform.Find("Blockage").gameObject;
-            if(door != null) {
+            // GameObject door = transform.parent.transform.Find("Blockage").gameObject;
+            // if(door != null) {
+            //     door.GetComponent<BoxCollider2D>().enabled = false;
+            //     transform.GetComponent<SpriteRenderer>().enabled = false;
+            // }
+            // foreach(Transform child in transform.parent){
+            //     if(child.tag == "Door"){
+            //         child.GetComponent<BoxCollider2D>().enabled = false;
+            //         transform.GetComponent<SpriteRenderer>().enabled = false;
+            //     }
+            // }
+            foreach(GameObject door in connectedDoors){
                 door.GetComponent<BoxCollider2D>().enabled = false;
-                transform.GetComponent<SpriteRenderer>().enabled = false;
-            }        
+            }
+            transform.GetComponent<SpriteRenderer>().enabled = false;        
         }
     }
 }
