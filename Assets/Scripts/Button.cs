9697ed6d2877a33b9417f62ea89b1498a43745c5
Liioni <40169289+Liioni@users.noreply.github.com>
2021-09-09 10:17:36 +0200

    added time enabled doors

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+    [SerializeField] int secondsInactive;
+    SpriteRenderer sr;
+    Color activeColor;
+    Color inactiveColor;
+
+    private void Start() {
+        sr = GetComponent<SpriteRenderer>();
+        activeColor = Color.green;
+        sr.color = activeColor;
+        inactiveColor = Color.red;
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag == "Player"){
-            transform.gameObject.SetActive(false);      
+            if(secondsInactive > 0){
+                StartCoroutine(ReActivate(secondsInactive));
+            }
+            this.transform.gameObject.GetComponent<Collider2D>().enabled = false;
+            sr.color = inactiveColor;      
         }
     }
+
+    IEnumerator ReActivate(int seconds){
+        yield return new WaitForSeconds(seconds);
+        this.transform.gameObject.GetComponent<Collider2D>().enabled = true;
+        sr.color = activeColor; 
+    }
+
 }
--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    SpriteRenderer sr;
+    Color activeColor;
+    Color inactiveColor;
+
+    private void Start() {
+        sr = GetComponent<SpriteRenderer>();
+        activeColor = Color.green;
+        inactiveColor = Color.red;
+        sr.color = inactiveColor;
+    }
+
     private void Update() {
         int countChild = 0;
         foreach(Transform child in transform){
-            if(child.gameObject.activeInHierarchy){
+            if(child.gameObject.GetComponent<Collider2D>().enabled){
                 countChild++;
             }
         }
         if(countChild <=0){
             transform.GetComponent<BoxCollider2D>().enabled = false;
+            sr.color = activeColor;
+        } else {
+            transform.GetComponent<BoxCollider2D>().enabled = true;
+            sr.color = inactiveColor;
         }
     }
 }
