92cd757c85a0384765d58cb2450f7ee06c6be0c5
Aiden Storey <git@storey.dev>
2021-09-09 20:45:29 -0400

    Add default scene loader

---
+++ Assets/Scripts/Scene/DefaultSceneLoader.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DefaultSceneLoader : MonoBehaviour
+{
+    public SceneName defaultScene;
+
+
+    // Lifecycle methods
+
+    void Awake()
+    {
+        SceneManager.LoadSceneAsync(this.defaultScene.ToString().ToLower(), LoadSceneMode.Additive);
+    }
+}
